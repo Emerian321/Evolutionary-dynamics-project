4d722cfc057e3766d4e9d57402b27b74ea21bb05
emerian320 <emile.merian@ulb.be>
2023-11-25 19:33:58 +0100

    part 2 start

--- part2.1.py
+++ part2.1.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from part2 import *
 
 Z = 100
--- part2.py
+++ part2.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
-import seaborn as sns
+
 
 import egttools as egt
 
